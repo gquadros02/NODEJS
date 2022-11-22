3d7db82f503a5cc8ca3f2a2c13b43f9afce53e2b
gquadros02 <110311960+gquadros02@users.noreply.github.com>
2022-11-22 14:35:57 +0000

    Atualizações

--- 06_Node_Mysql/index.js
+++ 06_Node_Mysql/index.js
@@ -86,12 +86,14 @@ app.get('/', (req, res) => {
 
 //Executar o servidor
 const comun = mysql.createConnection({
-    host: 'localhost',
+    host: '127.0.0.1',
     port: 3306,
     user: 'root',
     password: '',
     database: 'db_comum'
 });
+
+
 //Conectando com o banco de dados
 comun.connect((erro) => {
     if (erro) {
